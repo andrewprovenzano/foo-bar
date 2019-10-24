4232fab89b4e8aedaaf5367da7c3edd3508fb600
Andrew <provenzandrew@gmail.com>
2019-10-24 18:49:30 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template: '<h1>FooBar</h1>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'foo-bar';
+}
